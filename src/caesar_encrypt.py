34bc8adb793577a26f1e595e9fe9ec1dcdb7f633
Yishai Gantz <gantz.yishai@gmail.com>
2025-11-09 02:00:06 +0200

    my finel commit

--- src/caesar_encrypt.py
+++ src/caesar_encrypt.py
@@ -1,15 +1,32 @@
 from alphabets import get_alphabet
 from cyber_tools import frequency_analysis, plot_frequency
 
+def char_shift(lang, char, shift): #function to shift a single character by a given shifts amount
+        alphabet = get_alphabet(lang)
+        alphabet_len = len(alphabet)
+        result = ""
+        if char.lower() in alphabet: #check if character is in the alphabet
+            if char.islower():
+                index = alphabet.index(char.lower())
+                new_index = (index + shift + alphabet_len) % alphabet_len #double the length to avoid negative index
+                result = alphabet[new_index]
+            elif char.isupper():
+                index = alphabet.index(char.lower())
+                new_index = (index + shift + alphabet_len) % alphabet_len
+                result = alphabet[new_index].upper()
+            else:
+                result = char
+        return result
 
 
 def caesar_encrypt(lang, text, shift):
     alphabet = get_alphabet(lang)
     if alphabet is None:
         return text  # Return original text if language not supported
-
+    alphabet_len = len(alphabet)
     result = ""
-   
+    for char in text:
+        result += char_shift(lang, char, shift)
     return result
 
 
